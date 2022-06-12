fbf0eec230554022959affd59ba4bfa6e65aa15d
York Zhao <zys0307@outlook.com>
2022-06-12 14:09:18 +0800

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -1,5 +1,6 @@
 # This file is a part of TG-FileStreamBot
 # Coding : Jyothis Jayanth [@EverythingSuckz]
+# Translate : York Zhao [@York618]
 
 from pyrogram import filters
 from pyrogram.types import Message
@@ -9,5 +10,5 @@ from WebStreamer.bot import StreamBot
 @StreamBot.on_message(filters.command(["start", "help"]))
 async def start(_, m: Message):
     await m.reply(
-        f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
+        f'泥吼呀 {m.from_user.mention(style="md")}, 向我丢一个文件，然后得到属于你的直链吧！'
     )
